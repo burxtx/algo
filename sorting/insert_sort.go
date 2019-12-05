1015c16128937e5be62fc1108fce65543cbc30c5
unknown <tianxi@xiaomi.com>
2019-12-05 23:02:28 +0800

    add sorting: insert sorting

---
+++ sorting/insert_sort.go
@@ -0,0 +1,18 @@
+package insert_sort
+
+func InsertSort(xs []int32) []int32 {
+	for i := 1; i < len(xs); i++ {
+		x := xs[i]
+		j := i - 1
+		for {
+			if j >= 0 && x < xs[j] {
+				xs[j+1] = xs[j]
+				j = j - 1
+			} else {
+				xs[j+1] = x
+				break
+			}
+		}
+	}
+	return xs
+}
---
+++ sorting/insert_sort_test.go
@@ -0,0 +1,21 @@
+package insert_sort
+
+import (
+	"testing"
+)
+
+func TestInsertSort(t *testing.T) {
+	tests := []struct {
+		input  []int32
+		wanted string
+	}{
+		{[]int32{40, 8, 13, 20}, "8,13,20,40"},
+	}
+	for _, test := range tests {
+		res := InsertSort(test.input)
+
+		if res[0] != 20 {
+			t.Errorf("%v != %s", res, test.wanted)
+		}
+	}
+}
